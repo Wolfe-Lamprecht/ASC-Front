2df835a697dfffcdc42d4ea96e0f723a761b47ee
wolfet262 <tyler.wolfe262@outlook.com>
2021-03-15 13:07:39 -0700

    first commit

---
+++ asc/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react'
+import Header from './Components/Header.jsx'
+import Home from './Components/Home.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <h1>working</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ asc/src/Components/Users.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import User from './User'
+
+function Users() {
+
+    const [users, setUsers] = useState([])
+ 
+    const fetchUsers = () => {
+        const apiUrl = 'http://127.0.0.1:8000/users/';
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => setUsers(data)) 
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, []) 
+
+
+    return (
+        <Container
+        className="align-items-start">
+            <h5>Users Component</h5>
+            {users.map(user => 
+                <User
+                username={user.username} 
+                firstName={user.firstName}
+                lastName={user.lastName}
+                email={user.email}
+                status={user.status}
+                bio={user.bio}
+                password={user.password}
+                />
+            )}
+        </Container>
+    );
+}
+
+export default Users;
