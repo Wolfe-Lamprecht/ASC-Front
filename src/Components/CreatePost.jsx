c9a5265c7e5849bb87a9a9f8ca8a179a12553f1e
Caitlin <caitlinlamprecht@outlook.com>
2021-03-17 15:46:28 -0500

    updated dir

---
+++ src/Components/CreatePost.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import FormGroup from "react-bootstrap/FormGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
+function CreatePost(props) {
+  const [postTitle, setPostTitle] = useState("");
+  const [postContent, setPostContent] = useState("");
+
+  const handleChange = (event) => {
+    if (event.target.id === `postTitle`) {
+      setPostTitle(event.target.value);
+    } else if (event.target.id === `postBodyInput`) {
+      setPostContent(event.target.value);
+    }
+  };
+
+  const handleClick = (event) => {
+    if (event.target.id === `cancelPostButton`) {
+      setPostTitle("");
+      setPostContent("");
+    } else if (event.target.id === `createPostButton`) {
+    }
+  };
+
+  return (
+    <Form>
+      <h5>Create Post Component</h5>
+      <FormGroup>
+        <Form.Label className="float-left">Post Title:</Form.Label>
+        <FormControl
+          placeholder="e.g. 'My Feelings About...'"
+          aria-label="post-title"
+          id="postTitle"
+          onChange={handleChange}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Form.Label className="float-left">Your Post Here:</Form.Label>
+        <FormControl
+          as="textarea"
+          aria-label="post-body-input"
+          id="postBodyInput"
+          onChange={handleChange}
+        />
+      </FormGroup>
+      <Button
+        variant="primary"
+        className="ml-2 mb-3"
+        id="createPostButton"
+        onClick={handleClick}
+      >
+        Post
+      </Button>
+    </Form>
+  );
+}
+
+export default CreatePost;
---
+++ src/Components/User.jsx
@@ -0,0 +1,12 @@
+import { Card } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+
+function User(props) {
+    return (
+        <Container>
+
+        </Container>
+    );
+}
+
+export default User;
